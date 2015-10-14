0e53338a73740b949a51c40d08d983b8be6360cc
Masaya Murakami <m-murakami@ist.osaka-u.ac.jp>
2015-10-14 14:41:43 +0900

    assignment submission

--- lib/hello_trema.rb
+++ lib/hello_trema.rb
@@ -1,10 +1,17 @@
 # Hello World!
 class HelloTrema < Trema::Controller
   def start(_args)
-    logger.info 'Trema started.'
+    @switches = []
+    logger.info "Hi! #{self.class}"
   end
 
-  def switch_ready(datapath_id)
-    logger.info "Hello #{datapath_id.to_hex}!"
+  def switch_ready(dpid)
+    @switches << dpid.to_hex
+    logger.info "Hello #{dpid.to_hex}!"
+  end
+
+  def switch_disconnected(dpid)
+    @switches -= [dpid.to_hex]
+    logger.info "Bye #{dpid.to_hex}"
   end
 end
